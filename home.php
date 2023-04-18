ae72fd5f360756379011261f36591524cec65e8d
isabellapresto <isabella.presto@gmail.com>
2023-04-18 19:07:12 +0200

    final changes

--- home.php
+++ home.php
@@ -18,11 +18,7 @@ get_header();
 		
 		<nav class="navigation pagination">
 		<?php custom_pagination(); ?>
-								<!-- <h2 class="screen-reader-text">Inläggsnavigering</h2>
-								<a class="prev page-numbers" href="">Föregående</a>
-								<span class="page-numbers current">1</span>
-								<a class="page-numbers" href="">2</a>
-								<a class="next page-numbers" href="">Nästa</a> -->
+
 							</nav>
 						</div>
 						<aside id="secondary" class="col-xs-12 col-md-3">
