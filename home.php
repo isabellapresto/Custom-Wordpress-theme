73c7d3d7c87b212c3eadcffc9fa5eb8dfee06a57
isabellapresto <isabella.presto@gmail.com>
2023-04-18 17:00:15 +0200

    readme added

--- home.php
+++ home.php
@@ -26,75 +26,68 @@ get_header();
 							</nav>
 						</div>
 						<aside id="secondary" class="col-xs-12 col-md-3">
-							<div id="sidebar">
-								<ul>
-									<li>
-									<?php get_search_form();?>
-										<!-- <form id="searchform" class="searchform">
-											<div>
-												<label class="screen-reader-text">Sök efter:</label>
-												<input type="text" />
-												<input type="submit" value="Sök" />
-											</div>
-										</form> -->
-									</li>
-								</ul>
-								<ul role="navigation">
-									<li class="pagenav">
-										<h2>Sidor</h2>
-										<ul>
-											<li class="page_item current_page_item">
-												<a href="<?php echo home_url();?>">Blogg</a>
-											</li>
-											<li class="page_item">
-												<a href="<?php echo the_permalink(133);?>">Exempelsida</a>
-											</li>
-											<li class="page_item">
-												<a href="<?php echo home_url();?>">Kontakt</a>
-											</li>
-											<li class="page_item page_item_has_children">
-												<a href="<?php echo home_url();?>">Om mig</a>
-												<ul class="children">
-													<li class="page_item">
-														<a href="<?php echo home_url();?>">Intressen</a>
-													</li>
-													<li class="page_item page_item_has_children">
-														<a href="<?php echo home_url();?>">Portfolio</a>
-														<ul class="children">
-															<li class="page_item">
-																<a href="<?php echo home_url();?>">Projekt 1</a>
-															</li>
-														</ul>
-													</li>
-												</ul>
-											</li>
-											<li class="page_item">
-												<a href="<?php echo home_url();?>">Startsida</a>
-											</li>
-										</ul>
-									</li>
-									<li>
-										<h2>Arkiv</h2>
-										<ul>
-											<li>
-												<a href="arkiv.html">oktober 2016</a>
-											</li>
-										</ul>
-									</li>
-									<li class="categories">
-										<h2>Kategorier</h2>
-										<ul>
-											<li class="cat-item">
-												<a href="">Natur</a> (1)
-											</li>
-											<li class="cat-item">
-												<a href="">Okategoriserade</a> (3)
-											</li>
-										</ul>
-									</li>
-								</ul>
-							</div>
-						</aside>
+			<div id="sidebar">
+				<ul>
+					<li>
+						<?php get_search_form();?>
+					</li>
+				</ul>
+				<ul role="navigation">
+					<li class="pagenav">
+						<h2>Sidor</h2>
+						<ul>
+							<li class="page_item current_page_item">
+								<a href="<?php echo home_url(25);?>">Blogg</a>
+							</li>
+							<li class="page_item">
+							<a href="<?php echo the_permalink(133);?>">Exempelsida</a>
+							</li>
+							<li class="page_item">
+							<a href="<?php echo the_permalink(35);?>">Kontakt</a>
+							</li>
+							<li class="page_item page_item_has_children">
+							<a href="<?php echo the_permalink(130);?>">Om mig</a>
+							<ul class="children">
+							<li class="page_item">
+							<a href="<?php echo the_permalink(135);?>">Intressen</a>
+							</li>
+							<li class="page_item page_item_has_children">
+							<a href="<?php echo the_permalink(138);?>">Portfolio</a>
+							<ul class="children">
+							<li class="page_item">
+							<a href="<?php echo the_permalink(140);?>">Projekt 1</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+			<li class="page_item">
+				<a href="<?php echo home_url();?>">Startsida</a>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<h2>Arkiv</h2>
+		<ul>
+		<li>
+			<a href="arkiv.html">oktober 2016</a>
+		</li>
+	</ul>
+</li>
+<li class="categories">
+	<h2>Kategorier</h2>
+	<ul>
+		<li class="cat-item">
+			<a href="">Natur</a> (1)
+		</li>
+		<li class="cat-item">
+			<a href="">Okategoriserade</a> (3)
+		</li>
+	</ul>
+</li>
+</ul>
+</div>
+</aside>
 					</div>
 				</div>
 			</section>
